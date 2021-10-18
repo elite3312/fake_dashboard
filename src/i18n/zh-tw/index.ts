3a5542544e3e7dead03f09f3cd6772602b865431
Perry <perrywu12@gmail.com>
2021-10-18 13:08:37 +0800

    added zh-tw language pack

---
+++ src/i18n/zh-tw/index.ts
@@ -0,0 +1,32 @@
+
+
+
+
+
+export default {
+  failed: '操作失敗',
+  success: '操作成功',
+  //sidebar
+  frontend:'前台',
+  backend:'後台',
+  homepage:'首頁',
+  my_projects:'我的專案',
+  pending_application:'我的審核',
+  my_bills:'我的帳單',
+  //top
+  search:'搜尋',
+  bell:'提醒',
+  //usage
+  vol_group:'數據卷',
+  //list
+  list_my_projects:'專案項目',
+  proj_name:'名稱',
+  cost:'目前花費',
+  modules:'模組',
+  latest_activity:'最新活動',
+  owner:'擁有者',
+  pinned:'釘選',
+
+  //
+  new_proj:'創建新專案',
+};
