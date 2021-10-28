8bbeba80877ae17d7f1768317c8fad3b2286fbf3
Perry <perrywu12@gmail.com>
2021-10-28 22:03:09 +0800

    forgot to add a file

---
+++ src/components/svg_grey_crop_square.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg
+    width="16"
+    height="14"
+    viewBox="0 0 16 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M11 14H5C2.25 14 0 11.75 0 9V5C0 2.25 2.25 0 5 0H11C13.75 0 16 2.25 16 5V9C16 11.75 13.75 14 11 14Z"
+      fill="#CCCCCC"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'GreyCropSquare',
+  setup() {
+    return {};
+  },
+};
+</script>
